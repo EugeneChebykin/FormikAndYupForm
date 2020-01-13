0861ff02416eb30a00a65f37bc88870fcf6cd406
EugeneChebykin <respublicofrome@gmail.com>
2020-01-13 15:18:55 +0300

    test husky

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import FormikForm from './components/FormikForm';
+const a = 9;
 
 function App() {
   return <FormikForm />;
